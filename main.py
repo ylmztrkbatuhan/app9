dbd636bdba67c8608d63dd08dea2e3b76a3f6229
oboy21 <oboy11b@gmail.com>
2024-05-09 14:00:13 +0300

    Capture Webcam Video

---
+++ main.py
@@ -0,0 +1,22 @@
+import time
+
+import cv2
+
+video = cv2.VideoCapture(0)
+time.sleep(1)
+
+while True:
+    check, frame = video.read()
+    cv2.imshow("My vdeo", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
+video.release()
+
+
+
+
+
